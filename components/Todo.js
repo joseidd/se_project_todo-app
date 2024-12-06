c2ac9531ae2a35d2a4ac8cc58286ece8c72bdeeb
joseiddd <joseidd@outlook.com>
2024-12-06 12:33:55 -0500

    new update

--- components/Todo.js
+++ components/Todo.js
@@ -1,18 +1,36 @@
 class Todo {
-  constructor(data, selector) {
+  constructor(data, selector, handleCheck, handleDelete, handleTodo) {
     this._data = data;
     this._templateElement = document.querySelector(selector);
+    this.date = new Date(data.date);
+    this._handleCheck = handleCheck;
+    this._handleDelete = handleDelete;
+    this._handleTodo = handleTodo;
   }
 
-  _setEventListeners() {
-    this._todoCheckboxEl.addEventListener("change", () => {
-      this._data.completed = !this._data.completed;
-    });
+  isValid() {
+    return !isNaN(this.date);
+  }
+
+  _generateDate() {
+    if (this.isValid()) {
+      this._todoDate.textContent = `Due: ${this.date.toLocaleString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })}`;
+    }
   }
 
   _setEventListeners() {
     this._todoDeleteBtn.addEventListener("click", () => {
       this._todoElement.remove();
+      this._handleDelete(this._data.completed);
+      this._handleTodo(true);
+    });
+    this._todoCheckboxEl.addEventListener("change", () => {
+      this._data.completed = !this._data.completed;
+      this._handleCheck(this._data.completed);
     });
   }
 
@@ -32,23 +50,11 @@ class Todo {
     const todoNameEl = this._todoElement.querySelector(".todo__name");
 
     this._todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
-
+    this._todoDate = this._todoElement.querySelector(".todo__date");
     todoNameEl.textContent = this._data.name;
 
-    this._dueDate = new Date(this._data.date);
-    if (!isNaN(this._dueDate)) {
-      this._todoDate = this._todoElement.querySelector(".todo__date");
-      this._todoDate.textContent = `Due: ${this._dueDate.toLocaleString(
-        "en-US",
-        {
-          year: "numeric",
-          month: "short",
-          day: "numeric",
-        }
-      )}`;
-    }
-
     this._generateCheckboxEl();
+    this._generateDate();
     this._setEventListeners();
 
     return this._todoElement;
---
+++ components/TodoCounter.js
@@ -0,0 +1,35 @@
+class TodoCounter {
+  // todos should be the array of initial todos
+  // selector is the selector for the counter text element
+  constructor(todos, selector) {
+    this._element = document.querySelector(selector);
+    this._completed = 0;
+    this._completed = todos.filter((todo) => todo.completed).length;
+    this._total = todos.length;
+    this._updateText();
+  }
+
+  updateCompleted = (increment) => {
+    if (increment) {
+      this._completed++;
+    } else {
+      this._completed--;
+    }
+    this._updateText();
+  };
+
+  updateTotal = (increment) => {
+    if (increment) {
+      this._total++;
+    } else {
+      this._total--;
+    }
+    this._updateText();
+  };
+
+  _updateText() {
+    this._element.textContent = `Showing ${this._completed} out of ${this._total} completed`;
+  }
+}
+
+export default TodoCounter;
